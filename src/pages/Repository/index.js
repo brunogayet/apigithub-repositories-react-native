c66f3a93c33bf4df72235ca8fbf5c4ca479c7bf5
Bruno Gayet <brunogayet@gmail.com>
2020-04-06 04:04:40 -0400

    GitHub mobile repositories (React Native)

---
+++ src/pages/Repository/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Browser } from './styles';
+
+function Repository({ navigation }) {
+  const repository = navigation.getParam('repository');
+
+  return <Browser source={{ uri: repository.html_url }} />;
+}
+
+Repository.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,
+  }).isRequired,
+};
+
+Repository.navigationOptions = ({ navigation }) => ({
+  title: navigation.getParam('repository').name,
+});
+
+export default Repository;
